29ee63bac695c926e71c23227d76e99815c7d92a
akbar tri wicaksono <akbartriwicaksono46@gmail.com>
2023-08-13 09:27:27 +0800

    pembuatan model

---
+++ app/Models/Laundry.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Laundry extends Model
+{
+    use HasFactory;
+    protected $table = '';
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'email',
+        'location',
+    ];
+}
---
+++ app/Models/Member.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    use HasFactory;
+    protected $table = '';
+    protected $fillable = [
+        'name',
+        'nik',
+        'address',
+        'phone',
+        'email',
+        'laundry_id',
+        'verify',
+        'user_id',
+    ];
+}
